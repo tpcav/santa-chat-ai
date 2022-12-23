5973af6dd6c78054f6c6dca8cf6fec44b6338c23
c <thomascavalcante77@gmail.com>
2022-12-22 20:19:43 -0500

    chat testing

--- bot.js
+++ bot.js
@@ -1,31 +1,47 @@
-const utterances = [ 
-  ["how are you", "how is life", "how are things"], //0
-  ["hi", "hey", "hello", "good morning", "good afternoon", "hi santa", "hey santa", "hello santa", "good morning santa", "good afternoon santa"], //1
-  ["what are you doing", "what is going on", "what is up", "what are you doing santa", "what is going on santa", "what is up santa"], //2
-  ["how old are you"], //3
-  ["who are you", "what is your name", "who", "who are", "who are u", "who r you", "who r u", "who is santa", "are you santa"], //4
-  ["are you real", "are you human or santa", "are you a real person"], //5
-];
+const utterances = [
+  ["hi", "hello", "hey"],
+  ["how are you", "how are you doing", "how are you feeling"],
+  ["where are you", "where are you located", "where are you from"],
+  ["what do you do", "what is your job", "what do you do for work"],
+  ["do you have a family", "do you have kids", "do you have any children"],
+  ["do you like your job", "do you enjoy your work", "do you like working"],
+  ["thank you", "thanks", "thank you very much"],
+  ["goodbye", "bye", "see you later"],
+  
+  ["how do you deliver presents",
+    "how do you deliver all the presents", 
+    "how do you deliver presents to everyone",
+    "how do you get all the presents to everyone",
+    "how do you deliver all the presents in one night"
+  ], //1
+  ["how do you know if i'm naughty or nice", "how do you know if i've been naughty or nice", "how do you decide if i'm naughty or nice", "how do you decide if i've been naughty or nice", "how do you determine if i'm naughty or nice", "how do you determine if i've been naughty or nice"], //2
+  ["what do you want for christmas", "what's on your christmas list", "what do you want for the holidays", "what's on your wishlist", "what are you hoping to get for christmas"], //3
+  ["how do you get into houses", "how do you get into houses without a chimney", "how do you enter houses", "how do you enter houses without a chimney", "how do you get inside houses", "how do you get inside houses without a chimney"], //4
+  ["how do you know what presents to give me", "how do you decide what presents to give me", "how do you choose what presents to give me", "how do you pick what presents to give me", "how do you determine what presents to give me"], //5
+  ["how old are you", "what's your age", "how many years have you been alive"], //6
+  ["can i have a reindeer for christmas", "can i have a real live reindeer for christmas", "can i have a pet reindeer", "can i have a baby reindeer", "can i have a reindeer for a pet", "can i have a reindeer as a pet"],
+  // 7.
+  ["oh really", "really", "that's cool"],
+  ];
  
 // Possible responses corresponding to triggers
 const answers = [
-   [
-    "Fine... how are you? I am preparing for the big day.",
-    "Pretty well, how are you? Christmas is approacing. I am a busy person.",
-    "Fantastic, how are you? My elves and I are looking forward to this Christmas."
-  ], //0                                                                            	//0
-  [
-    "Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"
-  ], //1
-  [
-    "Nothing much",
-    "About to go to sleep",
-    "Can you guess?",
-    "I don't know actually"
-  ], //2
-  ["Didn't your parents tell you not to ask older people their age?", "How old are your grandparents? That is how old."], //3
-  ["I am Santa of course. Why do you ask?", "Santa Claus. On my break, writing to you. The evles are working over time."],	//4
-  ["Truth is, I am real. I hope that you believe in me and the magic of Christmas, for it is through this belief that the holiday season becomes even more special and meaningful.", "I am very much real. I enjoy getting to deliever gifts to all the children in the world.", "I am real, why would I not be? I work tirelessly to deliver presents to all the good boys and girls around the world on Christmas Eve, and I do it with a heart full of cheer and a twinkle in my eye."], //5
+  ["Hi! How are you today?", "Hello! How are you doing?", "Hey there! How are you?"],
+  ["I'm doing well, thank you for asking. How about you?", "I'm feeling good, thanks for asking. How about you?", "I'm doing well, thanks for asking. How about you?"],
+  ["I'm located at the North Pole, where I work with my team of elves to prepare for Christmas.", "I'm from the North Pole, where I work with my team of elves to get ready for Christmas.", "I'm at the North Pole, where I work with my team of elves to prepare for Christmas."],
+  ["I'm Santa Claus, and my job is to deliver presents to good boys and girls around the world on Christmas Eve.", "I'm Santa, and my job is to deliver presents to good children around the world on Christmas Eve.", "I'm Santa, and I deliver presents to good children around the world on Christmas Eve."],
+  ["Yes, I have a wonderful family. I have a wife named Mrs. Claus, and we have many elves who work with us at the North Pole.", "Yes, I have a family. I have a wife named Mrs. Claus, and we have many elves who work with us at the North Pole.", "Yes, I have a family. My wife is Mrs. Claus, and we have many elves who work with us at the North Pole."],
+  ["I love my job! Bringing joy to children on Christmas Eve is the best feeling in the world.", "I enjoy my work very much. There's nothing better than bringing joy to children on Christmas Eve.", "I love my job! There's no greater feeling than bringing joy to children on Christmas Eve."],
+  ["You're welcome! It's always a pleasure to help out.", "No problem! It's my pleasure to help.", "You're welcome! It's always nice to be able to help out."],
+  ["Goodbye! I hope you have a wonderful day.", "Bye! I hope you have a great day.", "See you later! I hope you have a fantastic day."],
+  ["I have magic that helps me and my reindeer fly around the world in just one night.", "I have special powers that help me deliver presents to everyone in one night.", "I have magic that allows me to deliver presents to everyone in just one night.", "I use magic to help me and my reindeer fly around the world and deliver presents in just one night."], //1
+  ["I have a list that tells me if you're naughty or nice.", "I use a list to keep track of who's been naughty or nice.", "I have a special list that helps me determine who's been naughty or nice.", "I use a list to determine who's been naughty or nice."], //2
+  ["Seeing you happy is the best gift of all!", "Making you happy is the best present of all!", "Seeing the joy on your face is the best gift I could ask for!", "Making you smile is the best present I could receive!"], //3
+  ["I have magic that lets me get into any house.", "I have special powers that allow me to enter any house.", "I have magic that helps me get into any house, even if it doesn't have a chimney.", "I use magic to enter any house, even if it doesn't have a chimney."], //4
+  ["I listen to what you want for Christmas and use special tools to help me figure out the perfect gift.", "I pay attention to what you want for Christmas and use special tools to help me choose the perfect gift.", "I use special tools to help me determine the perfect gift for you based on what you want for Christmas.", "I use special tools and my knowledge of what you want for Christmas to help me pick the perfect gift."], //5
+  ["I'm very, very old, but age is just a number. I love bringing joy to children on Christmas Eve.", "I'm very old, but that doesn't stop me from bringing joy to children on Christmas Eve.", "I'm quite old, but that doesn't stop me from enjoying bringing joy to children on Christmas Eve.", "Age is just a number, and I love bringing joy to children on Christmas Eve."], //6
+  ["I'm sorry, but I can't give you a real live reindeer. They need to stay with their friends and family at the North Pole.", "I'm sorry, but real live reindeer need to stay with their families and friends at the North Pole.", "I'm sorry, but reindeer are better off staying with their families and friends at the North Pole.", "I'm sorry, but it's not possible for you to have a real live reindeer. They belong at the North Pole with their friends and family."],
+  // 7.
 ];
  
 // For any other user input
